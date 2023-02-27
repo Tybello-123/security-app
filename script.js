058dcb6624558f61b0fa4d7d0cd2885f0007371a
Bello Toybat <98482637+Tybello-123@users.noreply.github.com>
2023-02-27 23:51:17 +0100

    Second Commit

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// let incident = document.getElementById("textarea").value
-// let case2 = document.getElementById("case2")
+document.getElementById("submit").addEventListener("click",enterIssue);
 
 
-// function enterIssue(){
-//     case2.textContent = incident
+function enterIssue() {
+
+    let listItem = document.createElement("LI");  
+    //create a list item using document.createElement
+
+    let incident = document.getElementById("textarea").value;
+    //get the value of the textarea element input
+
+    listItem.innerHTML = incident;
+    //assign the value of the list item to the incident
+
+  
+    document.getElementById("crime-list").appendChild(listItem);
+    //get the id of the parent element and append the child
+
+ clearInput()
+}
+enterIssue();
+
+function clearInput(){
+  let clear = document.getElementById("textarea");
+    if(clear.value !="") {
+        clear.value = "";
+      
+    }
+}
+
+// if (process.browser) {
+//     console.log("code is running in a browser environment")
     
-// }
-// enterIssue();
+//   } else {
+//     console.log("code is running in a Node.js environment")
+//   }
 
-if (process.browser) {
-    console.log("code is running in a browser environment")
-    
-  } else {
-    console.log("code is running in a Node.js environment")
-  }
